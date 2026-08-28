767c8ffbfaae5aea19ec03537f273532d5a7bfc3
dheerjtomar2424-dev <dheerjtomar2424@gmail.com>
2026-08-28 20:55:41 +0530

    Time: 0 ms (100%), Space: 7.9 MB (80.27%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    int solve(int n, int row, int columns, int diag1, int diag2) {
+
+        if (row == n)
+            return 1;
+
+        int count = 0;
+
+        int available = ((1 << n) - 1) &
+                        ~(columns | diag1 | diag2);
+
+        while (available) {
+
+            int bit = available & -available;
+            available -= bit;
+
+            count += solve(
+                n,
+                row + 1,
+                columns | bit,
+                (diag1 | bit) << 1,
+                (diag2 | bit) >> 1
+            );
+        }
+
+        return count;
+    }
+
+    int totalNQueens(int n) {
+        return solve(n, 0, 0, 0, 0);
+    }
+};
