920216aeb70620b9bd7e7125c34de02af727d4e9
dheerjtomar2424-dev <dheerjtomar2424@gmail.com>
2026-08-29 22:36:51 +0530

    Time: 0 ms (100%), Space: 13.8 MB (98.75%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+
+        int size = 1 << n;
+        vector<int> ans;
+        ans.reserve(size);
+
+        for (int i = 0; i < size; i++) {
+            ans.push_back(i ^ (i >> 1));
+        }
+
+        return ans;
+    }
+};
